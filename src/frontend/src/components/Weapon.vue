2d1fb12ab3b6da50a7c7bc0e99e378199e21eb9a
LeeHyengTae <tedd911@icloud.com>
2020-05-21 09:00:59 +0900

    09:00

---
+++ src/frontend/src/components/Weapon.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Weapon</h1>
+</template>
+
+<script>
+    export default {
+        name: "Weapon"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div>
+        <v-app id="inspire">
+            <v-navigation-drawer
+                    v-model="drawer"
+                    app
+            >
+                <v-list dense>
+                    <v-list-item link>
+                        <v-list-item-action>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <router-link to="/"><v-list-item-title>Home</v-list-item-title></router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item link>
+                        <v-list-item-action>
+                            <v-icon>mdi-contact-mail</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <router-link to="/composition"><v-list-item-title>{{Composition}}</v-list-item-title></router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item link>
+                        <v-list-item-action>
+                            <v-icon>mdi-contact-mail</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                           <router-link to="/member"> <v-list-item-title>{{Member}}</v-list-item-title></router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item link>
+                        <v-list-item-action>
+                            <v-icon>mdi-contact-mail</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <router-link to="weapon"><v-list-item-title>{{Weapon}}</v-list-item-title></router-link>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+
+            <v-app-bar
+                    app
+                    color="indigo"
+                    dark
+            >
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                <v-toolbar-title>{{title}}</v-toolbar-title>
+            </v-app-bar>
+
+            <v-content><router-view/></v-content>
+            <v-footer
+                    color="indigo"
+                    app
+            >
+                <span class="white--text">&copy; 2019</span>
+            </v-footer>
+        </v-app>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+    export default {
+        name: "Home",
+        data: () =>({
+            drawer: null,
+            title: 'Home', Composition:'조직도', Member:'중대원 목록', Weapon:'화기목록'
+        }),
+        methods: {
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+        }
+    }
 </script>
+
+<style scoped>
+
+</style>
